3885348adceef57a74c2de73755654c571e99ca5
gkime <gkime91@gmail.com>
2023-10-26 14:33:49 -0700

    did merge sort

---
+++ algorithms/mergesort.js
@@ -0,0 +1,41 @@
+
+/**
+ * 
+ * @param {int[]} array 
+ */
+const mergeSort = (array) => {
+  // base case: arrays with 1 or 0 elements are sorted, return those
+  if (array.length <= 1) return array;
+  // recursive: split arrays in half until they contain one or 0 elements
+  const mid = Math.floor(array.length / 2);
+  const left = mergeSort(array.slice(0, mid));
+  const right = mergeSort(array.slice(mid));
+  // merge sorted left and right
+  return merge(left, right);
+  
+}
+
+/**
+ * 
+ * @param {int[]} left
+ * @param {int[]} right
+ */
+
+const merge = (left, right) => {
+  // declare sorted variable assigned empty array. 
+  const sorted = [];
+  // loop through elements while left and right
+  while (left.length && right.length) {
+    // compare left and right elements,
+    // push smaller, then larger to sorted
+    if (left[0] <= right[0]) {
+      sorted.push(left.shift());
+    } else (sorted.push(right.shift()));
+  };
+
+  return [...sorted, ...left, ...right];
+}
+
+const testArr = [0, 2, 1, 10];
+
+console.log(mergeSort(testArr));
