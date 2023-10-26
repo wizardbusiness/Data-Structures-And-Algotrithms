a4fae9a497b9fb6258e16d4d6281529ec88e2b77
gkime <gkime91@gmail.com>
2023-10-26 14:35:53 -0700

    added set method that handles collisions

--- dataStructures/706HashMap.js
+++ dataStructures/706HashMap.js
@@ -38,16 +38,17 @@ class SimpleHashMap {
  */
 
 class HashMap {
-  constructor() {
+  constructor(size) {
     // array or linked list
-    this.hashObj = new Array(23);
+    this.table = new Array(size);
+    this.emptyBuckets = this.table.length;
   }
   /**
    * Takes in a value and returns a key
    * @param {string} val
    * @returns {index}
    */
-  hashVal(string, tableLength) {
+  hash(string, tableLength) {
     let hash = 17;
     for (let i = 0; i < string.length; i++) {
       // if hash for a char is 0, any subsequent hash value for the string will also be 0.
@@ -57,9 +58,39 @@ class HashMap {
     }
     return hash;
   }
+
+  set(val) {
+    // add value to hashmap
+    const hash = this.hash(val, this.table.length);
+    // if hash table is 3/4 full, double the size of the hash table
+    if (this.emptyBuckets < Math.floor(this.table.length * 3/4)) {
+      this.table = [...this.table, ...new Array(this.table.length)];
+      this.emptyBuckets = this.table.length;
+    }
+    // if collisions
+    if (this.table[hash]) {
+      // add array at collision point.
+      if (Array.isArray(this.table[hash])) this.table[hash].push(val)
+      else this.table[hash] = [this.table[hash], val];
+      return;
+    };
+
+    this.table[hash] = val; 
+    this.emptyBuckets--;
+    return;
+  }
 }
 
-const hash = new HashMap();
+const hash = new HashMap(6);
 
-hash.hashVal("firstName", 21);
+
+const addValuesToHash = (arr, hash) => {
+  arr.forEach(val => hash.set(val));
+}
+
+const strings = ['the', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog']
+
+addValuesToHash(strings, hash);
+
+console.log(hash.table)
 
