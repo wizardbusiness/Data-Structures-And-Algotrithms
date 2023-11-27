306f99e4459f95e00edc173f1428bd4a82c6c513
Gabriel <gkime91@gmail.com>
2023-11-26 23:52:13 -0800

    pratice binarySearch

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -1,36 +1,34 @@
-// divide array in half repeatedly according to whether passed in value is greater or less than that of middle value
-// if passed in is equal to middle, return true
-// if array is empty, return false
+/** Divide and Conquer. Divide array repeatedly in half until either middle element is found or array is empty */
 
 /**
  *
- * @param {number[]} arr sorted array
+ * @param {number[]} arr
  * @param {number} val
  * @returns {boolean}
- * recursively divides array in two until array is empty or passed in value is equal to the arrays middle value
  */
 const binarySearch = (arr, val) => {
-  // base
-  // if array is empty, return false
+  // handle edge cases
+  if (typeof val !== "number") return "invalid value";
+  if (!Array.isArray(arr)) return "no valid array found";
+  // base case
+  // if array is empty return false
   if (arr.length === 0) return false;
-  // choose middle index
-  const midIndex = Math.floor((arr.length - 1) / 2);
-  // if val equals middle value return true
-  if (val === arr[midIndex]) return true;
-  // else if val is greater than middle value slice array from middle to end
-  else if (val > arr[midIndex]) {
-    const greaterThan = arr.slice(midIndex + 1, arr.length);
-    console.log(greaterThan);
-    return binarySearch(greaterThan, val);
-    // else if val is less than mid val slice array from start to middle
-    // recurse binary search with sliced array
+  // get mid index
+  const midIndex = Math.floor(arr.length / 2);
+  // if mid equals val, return true
+  if (typeof arr[midIndex] !== "number") return "array contains invalid values";
+  if (arr[midIndex] === val) return true;
+  // recursive
+  // if val is greater than mid, recurse with right half of array
+  if (val > arr[midIndex]) {
+    const right = arr.slice(midIndex + 1, arr.length);
+    return binarySearch(right, val);
   } else if (val < arr[midIndex]) {
-    const lessThan = arr.slice(0, midIndex);
-    console.log(lessThan);
-    return binarySearch(lessThan, val);
+    // else if val is less than id, recurse with left half of array
+    const left = arr.slice(0, midIndex);
+    return binarySearch(left, val);
   }
 };
 
-const test = [1, 2, 3, 4, 5, 20, 100, 101, 102, 105];
-
-console.log(binarySearch(test, 102));
+const test = [1, 2, 3, 4, 5];
+console.log(binarySearch(test, 1)); // true
