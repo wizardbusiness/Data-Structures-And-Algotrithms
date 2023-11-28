b0cc04da01c7b3282023af2da84198dd5e96b466
Gabriel <gkime91@gmail.com>
2023-11-28 00:01:05 -0800

    practice

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -1,4 +1,3 @@
-
 // const partitionArray = (arr) => {
 //   // choose pivot
 //   const pivot = arr[arr.length - 1];
@@ -7,7 +6,7 @@
 //   // set i and j to the beginning and end of array
 //   let i = 0;
 //   let j = partition.length - 1;
-//   // while i is less than j 
+//   // while i is less than j
 //   while (i < j) {
 //     // if el at i is larger than pivot val and el at j is less than pivot val
 //     if (partition[i] > pivot && partition[j] < pivot) {
@@ -21,7 +20,7 @@
 //     // otherwise move i and j towards each other
 //     if (partition[j] > pivot) {
 //       j--
-//     } 
+//     }
 //     if (partition[i] < pivot) {
 //       i++
 //     }
@@ -32,31 +31,41 @@
 
 // const split = (arr, pivot) => {
 //   if (arr.length <= 2) return arr;
-//   return split() 
+//   return split()
 // }
 
 // const quickSort = (arr) => {
 
 // }
 // const test = [3, 4, 8, 1, 7, 5];
-
-const quicksort = (arr) => {
+/**
+ *
+ * @param {number []} arr
+ * @returns {number []}
+ */
+const quickSort = (arr) => {
+  // base case
+  // if array length is 1 return as sorted
   if (arr.length <= 1) return arr;
-  const pivot = arr[arr.length - 1];
-  const leftArr = [];
-  const rightArr = [];
-  arr.forEach(el => {
-    if (el < pivot) leftArr.push(el);
-    else if (el > pivot) rightArr.push(el);
+  // recursive
+  // pick a pivot
+  const pivot = arr[arr.length - 1]; // easy pivot pick, should be random to prevent worst case though
+  // sort els less than into left and els greater than into right arrays
+  const l = [];
+  const r = [];
+  arr.forEach((el) => {
+    if (el < pivot) l.push(el);
+    else if (el > pivot) r.push(el);
   });
+  // quicksort left and right
 
-  const left = quicksort(leftArr);
-  const right = quicksort(rightArr);
+  const left = quickSort(l);
+  const right = quickSort(r);
 
+  // return arrays back up, spread left, pivot and right into new array
   return [...left, pivot, ...right];
 };
 
-const test = [3, 4, 8, 1, 7, 5, 10, 13, 0];
-console.log(quicksort(test));
+const unsorted = [0, 5, 3, 2, 1, 10, 100, -100, 200, 19, 2, -200];
 
-
+console.log(quickSort(unsorted));
