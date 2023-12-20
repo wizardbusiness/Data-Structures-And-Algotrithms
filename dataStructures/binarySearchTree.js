224f5521e553bdf47c05d9b61fa54d9a78f9f614
Gabriel <gkime91@gmail.com>
2023-12-20 12:42:57 -0800

    practice delete method

--- dataStructures/binarySearchTree.js
+++ dataStructures/binarySearchTree.js
@@ -6,7 +6,6 @@
  * @property { Node | null } right - The right child node (default is null)
  * @property { number } count - The value count the node holds (for duplicate values)
  */
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -20,7 +19,6 @@ class Node {
  * @class BST
  * @property {object} root - the binary search tree object
  */
-
 class BST {
   constructor() {
     this.root = null;
@@ -31,7 +29,6 @@ class BST {
    * @param {number} value - the value to be added to the tree
    * @returns {void}
    */
-
   add(value) {
     let currNode = this.root;
     // if root is null, add root node
@@ -71,17 +68,51 @@ class BST {
    * @returns {void}
    *
    */
+  delete(value, currNode = this.root) {
+    // base case
+    // if currNode is null return -1
+
+    // recursive
+    // if value is less than currNode value
+      // recurse currNode's left
+    // else if value is greater than currNode value
+      // recurse currNode's right
+    // else if value equals currNode value
+    // if leaf node
+    // if currNodes 
+      // repoint parents pointer to null
+    // if one child
+      // repoint parents pointer to removed node's child
+      // right node only
+        // repoint parents right to childs right
+      // left node only
+        // repoint parents right to childs left
+    // if two children
+      // assign temp pointer to left
+      // assign temp pointer to right
+      // point parents right to childs right
+      // traverse from temp pointer at right passing in root value of left
+      // reattach left
+  }
 
   /**
    * Finds a value in the binary search tree
    * @param {number} - value
    * @returns {boolean}
    */
-
-  /**
-   * Prints the nodes in the binary search tree
-   * @returns {string}
-   */
+  find(value) {
+    let currNode = this.root;
+    while (currNode) {
+      if (value === currNode.value) {
+        return true;
+      } else if (value < currNode.value) {
+        currNode = currNode.left;
+      } else if (value > currNode.value) {
+        currNode = currNode.right;
+      }
+    }
+    return false;
+  }
 }
 
 const bst = new BST();
@@ -90,4 +121,7 @@ const values = [10, 9, 2, 4, 5, 10, 23, 2];
 function addValuesToBst(values, bst) {
   values.forEach((value) => bst.add(value));
 }
+
 addValuesToBst(values, bst);
+
+bst;
