0fd0f32398f4da3681568d8452d64892808bb8ec
Gabriel <gkime91@gmail.com>
2023-12-29 21:38:21 -0800

    mergesort practice

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -1,39 +1,40 @@
 /**
  *
- * @param {number[]} array
+ * @param {[]} array - takes an unsorted array
+ * @returns {[]} - returns a sorted array
  */
 const mergeSort = (array) => {
-  // base case: arrays with 1 or 0 elements are sorted, return those
-  if (array.length <= 1) return array;
-  // recursive: split arrays in half until they contain one or 0 elements
-  const mid = Math.floor(array.length / 2);
-  const left = mergeSort(array.slice(0, mid));
-  const right = mergeSort(array.slice(mid));
-  // merge sorted left and right
-  return merge(left, right);
-};
+  function bifurcate(array) {
+    // split the array in two until the base case is met
+    // base case: array has 0 or 1 elements
+    console.log(array);
+    if (array.length <= 1) return array;
 
-/**
- *
- * @param {number[]} left
- * @param {number[]} right
- */
+    // recursive: bifurcate the array recursively
+    const mid = Math.floor(array.length / 2);
+    const left = bifurcate(array.slice(0, mid));
+    const right = bifurcate(array.slice(mid));
 
-const merge = (left, right) => {
-  // declare sorted variable assigned empty array.
-  const sorted = [];
-  // loop through elements while left and right
-  while (left.length && right.length) {
-    // compare left and right elements,
-    // push smaller, then larger to sorted
-    if (left[0] <= right[0]) {
-      sorted.push(left.shift());
-    } else sorted.push(right.shift());
+    return merge(left, right);
   }
-
-  return [...sorted, ...left, ...right];
+  function merge(left, right) {
+    // while left and right
+    const sorted = [];
+    while (left.length && right.length) {
+      // compare elements in each array
+      // sort smaller one left,
+      // sort larger right
+      if (left[0] < right[0]) {
+        sorted.push(left.shift());
+      } else {
+        sorted.push(right.shift());
+      }
+    }
+    return [...sorted, ...left, ...right];
+    // merge left, remainder and right
+  }
+  console.log(bifurcate(array));
+  return bifurcate(array);
 };
 
-const testArr = [0, 2, 1, 10];
-
-console.log(mergeSort(testArr));
+mergeSort([1, 29, 19, 10, 22, 9, 0, 3]);
