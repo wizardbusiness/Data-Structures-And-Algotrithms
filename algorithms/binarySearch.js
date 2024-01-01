a0239164fdcf3823d770ab8e8f4d318cb1f2494d
Gabriel <gkime91@gmail.com>
2024-01-01 00:13:57 -0800

    practice binary search algo

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -10,10 +10,19 @@ const binarySearch = (array, target) => {
   // if target is found, return index
   // mid index
   // split array in half
+
   const midIndex = Math.floor(array.length / 2);
   if (array[midIndex] === target) return midIndex;
-  binarySearch(array.slice(0, midIndex), target);
-  binarySearch(array.slice(midIndex + 1), target);
-
-  return -1;
+  console.log(array[midIndex]);
+  if (target < array[midIndex]) {
+    const left = array.slice(0, midIndex);
+    return binarySearch(left, target);
+  } else if (target > array[midIndex]) {
+    const right = array.slice(midIndex + 1, array.length);
+    return binarySearch(right, target);
+  }
 };
+
+const testArray = [1, 2, 4, 6, 10, 12, 1000];
+
+console.log(binarySearch(testArray, 1));
