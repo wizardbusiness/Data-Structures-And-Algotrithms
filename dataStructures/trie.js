906a4325abbf9ec49c833c94eada2055d71016d4
Gabriel <gkime91@gmail.com>
2024-01-03 18:20:52 -0800

    practice trie

--- dataStructures/trie.js
+++ dataStructures/trie.js
@@ -1,18 +1,16 @@
 /**
  * @class TrieNode
- *
  */
 
 class TrieNode {
   constructor() {
     this.children = {};
-    this.wordEnd = false;
+    this.stringEnd = false;
   }
 }
 
 /**
  * @class Trie
- * @property {object} root
  */
 
 class Trie {
@@ -22,68 +20,61 @@ class Trie {
 
   /**
    * @method insert
-   * @param {string} string - the string to be inserted
-   * @returns {void}
+   * @param {string} string - string to be inserted
    */
 
   insert(string) {
     let currNode = this.root;
-    // insert string one char at a time
-    // if char does not exist at currnode, insert it
-    [...string].forEach((char) => {
-      if (!(char in currNode.children)) {
+    for (const char of string) {
+      if (char in currNode.children) {
+        currNode = currNode.children[char];
+      } else {
         currNode.children[char] = new TrieNode();
         currNode = currNode.children[char];
-      } else if (char in currNode.children) {
-        currNode = currNode.children[char];
       }
-    });
-    currNode.wordEnd = true;
+    }
+    currNode.stringEnd = true;
   }
 
   /**
    * @method startsWith
-   * @param {string} string - string that a word starts with in the trie
-   * @returns {boolean}
+   * @param {string} substr - substr to search for
    */
 
-  startsWith(string) {
+  startsWith(substr) {
     let currNode = this.root;
-    for (let char of string) {
-      if (!(char in currNode.children)) {
-        return false;
-      } else if (char in currNode.children) {
-        currNode = currNode.children[char];
-      }
+
+    for (const char of substr) {
+      if (!(char in currNode.children)) return false;
+      else if (char in currNode.children) currNode = currNode.children[char];
     }
+
     return true;
   }
 
   /**
    * @method contains
-   * @param {string} string - string that matches a full word in the trie
-   * @returns {boolean}
+   * @param {string} string - string to search for
    */
 
   contains(string) {
     let currNode = this.root;
-    for (let char of string) {
-      if (!(char in currNode.children)) {
-        return false;
-      } else if (char in currNode.children) currNode = currNode.children[char];
+
+    for (const char of string) {
+      if (!(char in currNode.children)) return false;
+      else if (char in currNode.children) currNode = currNode.children[char];
     }
-    return currNode.wordEnd;
+
+    return currNode.stringEnd;
   }
 }
 
 const trie = new Trie();
-
-trie.insert("cat");
-trie.insert("cast");
-trie.insert("rose");
-
-console.log(trie.startsWith("ro"));
-console.log(trie.startsWith("x"));
-console.log(trie.contains("rose"));
-
+trie.insert("fox");
+trie.insert("foxy");
+trie.insert("quick");
 console.dir(trie, { depth: null });
+console.log(trie.startsWith("fo"));
+console.log(trie.startsWith(" "));
+
+console.log(trie.contains("quick"));
