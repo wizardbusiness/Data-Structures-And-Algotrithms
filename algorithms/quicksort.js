b2dfcdbf7af6313e6f7e0ca30308d2b924ce794c
Gabriel <gkime91@gmail.com>
2024-01-09 23:22:56 -0800

    practicing lomuto quicksort

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -9,22 +9,43 @@ const unsorted = [0, 5, 3, 2, 1, 10, 100, -100, 200, 19, 2, -200];
  */
 
 const lomutoPartition = (array, low, high) => {
+  // const pivot = array[high];
+  // let i = low;
+  // let j = high;
+  // while (i <= j) {
+  //   while (array[i] < pivot) {
+  //     i++;
+  //   }
+  //   while (arr[j] > pivot) {
+  //     j--;
+  //   }
+  //   if (i <= j) {
+  //     let temp = arr[i];
+  //     arr[i] = arr[j];
+  //     arr[j] = temp;
+  //     i++;
+  //     j--;
+  //   }
+  // }
   const pivot = array[high];
-  let i = low;
-  let j = high;
-  while (i <= j) {
-    while (array[i] < pivot) {
-      i++;
+  // move towards the center of the partition to cover all elements.
+  while (low <= high) {
+    // find the first element that's greater than pivot on the left.
+    while (array[low] < pivot) {
+      low++;
     }
-    while (arr[j] > pivot) {
-      j--;
+    // find the first element that's less than pivot on the right.
+    while (array[high] > pivot) {
+      high--;
     }
-    if (i <= j) {
-      let temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-      i++;
-      j--;
+    // we're just making sure that we're in bounds
+    if (low <= high) {
+      // swap the elements at low and high
+      // so the one that's less than the pivot on the right is now on the left,
+      // and the one that's greater than the pivot on the right is now on the right;
+      let temp = array[low];
+      array[low] = array[high];
+      array[high] = temp;
     }
   }
 };
