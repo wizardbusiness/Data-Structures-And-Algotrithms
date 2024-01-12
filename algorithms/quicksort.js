212b785f8be408178dfd0d96a96095ed366a889e
Gabriel <gkime91@gmail.com>
2024-01-12 12:53:14 -0800

    quicksort practice

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -1,43 +1,46 @@
 const unsorted = [0, 5, 3, 2, 1, 10, 100, -100, 200, 19, 2, -200];
-const unsorted2 = [20, ]
+const unsorted2 = [20];
 
 /**
- * @function quicksort
- * @param {int[]} array - unsorted array
- * @param {int} start - start of partition
- * @param {int} end - end of partition
+ * @function quicksort - sorts an array of ints in O(nlogn) time, using pivot element and divide and conquer
+ * @param {int[]} array -unsorted array of ints
+ * @param {int} start - start index
+ * @param {int} end - end index
  */
 
 function quicksort(array, start, end) {
   if (start >= end) return;
 
-  const partitionIndex = lomutoPartition(array, start, end);
+  const partitionIndex = partition(array, start, end);
 
   quicksort(array, start, partitionIndex - 1);
   quicksort(array, partitionIndex + 1, end);
 }
 
 /**
- * @function lomutoPartition
- * @param {int[]} array - unsorted array
- * @param {int} start - start of partition
- * @param {int} end - end of partition
+ * @function partition - partitions an array in O(n)
+ * @param {int[]} array - array to be partitioned
+ * @param {int} start - partition beginning
+ * @param {int} end - partition end
+ *
  */
 
-function lomutoPartition(array, start, end) {
-  const pivotValue = array[end];
+function partition(array, start, end) {
+  const pivot = array[end];
+
   let pivotIndex = start;
 
   for (let i = start; i < end; i++) {
-    if (array[i] < pivotValue) {
+    if (array[i] < pivot) {
       [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
       pivotIndex++;
     }
   }
+
   [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
-  console.log(array);
   return pivotIndex;
 }
 
 quicksort(unsorted, 0, unsorted.length - 1);
+
 console.log(unsorted);
