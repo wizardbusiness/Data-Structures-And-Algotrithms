4524e9c92b2db1d3adf37e6d2ca8488f4c182484
Gabriel <gkime91@gmail.com>
2024-01-15 00:10:57 -0800

    quicksort practice

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -10,8 +10,41 @@ const unsorted2 = [20];
  * @param {int} end - partition end
  */
 
+function quicksort(array, start, end) {
+  // base case
+  if (start >= end) return;
+  const partitionIndex = partition(array, start, end);
+  quicksort(array, start, partitionIndex - 1);
+  quicksort(array, partitionIndex + 1, end);
+}
+
 /**
  * @function partition
  * @description partitions the array being sorted into integers less and greater than pivot value
+ * @param {int[]} array - array being partitioned
+ * @param {int} start - partition start
+ * @param {int} end - partition end
  * @returns {int} returns partition index
  */
+
+function partition(array, start, end) {
+  const pivot = array[end];
+  let partitionIndex = start;
+
+  console.log(partitionIndex);
+
+  for (let i = start; i < end; i++) {
+    if (array[i] < pivot) {
+      [array[i], array[partitionIndex]] = [array[partitionIndex], array[i]];
+      partitionIndex++;
+    }
+  }
+
+  [array[partitionIndex], array[end]] = [array[end], array[partitionIndex]];
+
+  return partitionIndex;
+}
+
+quicksort(unsorted, 0, unsorted.length - 1);
+
+console.log(unsorted);
