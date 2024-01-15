66034e4a9d58b06c5c88e6174a4ca8389743a96f
Gabriel <gkime91@gmail.com>
2024-01-15 10:56:37 -0800

    practice quicksort

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -2,49 +2,42 @@ const unsorted = [0, 5, 3, 2, 1, 10, 100, -100, 200, 19, 2, -200];
 const unsorted2 = [20];
 
 /**
- * @function quicksort
- * @description Sorts an unsorted array of integers from smallest to greatest in O(nlogn) time complexity
- * using pivot and divide and conquer partitioning.
- * @param {int[]} array - unsorted array
- * @param {int} start - partition start
- * @param {int} end - partition end
+ * @function quicksort sorts an unsorted array of ints using partitioning and divide and conquer
+ * @param {int[]} array of integers
+ * @param {int} start partition start
+ * @param {int} end partition end
  */
 
-function quicksort(array, start, end) {
-  // base case
+function quicksort(array, start = 0, end = array.length - 1) {
   if (start >= end) return;
+
   const partitionIndex = partition(array, start, end);
+
   quicksort(array, start, partitionIndex - 1);
   quicksort(array, partitionIndex + 1, end);
 }
 
 /**
- * @function partition
- * @description partitions the array being sorted into integers less and greater than pivot value
- * @param {int[]} array - array being partitioned
- * @param {int} start - partition start
- * @param {int} end - partition end
- * @returns {int} returns partition index
+ * @function partition partitions an unsorted array around a pivot
+ * @param {int[]} array unsorted arrray
+ * @param {int} start partition start
+ * @param {int} end partition end
  */
 
 function partition(array, start, end) {
   const pivot = array[end];
-  let partitionIndex = start;
-
-  console.log(partitionIndex);
-
+  let pivotIndex = start;
   for (let i = start; i < end; i++) {
     if (array[i] < pivot) {
-      [array[i], array[partitionIndex]] = [array[partitionIndex], array[i]];
-      partitionIndex++;
+      [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+      pivotIndex++;
     }
   }
+  [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
 
-  [array[partitionIndex], array[end]] = [array[end], array[partitionIndex]];
-
-  return partitionIndex;
+  return pivotIndex;
 }
 
-quicksort(unsorted, 0, unsorted.length - 1);
+quicksort(unsorted);
 
 console.log(unsorted);
