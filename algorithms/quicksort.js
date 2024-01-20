ac3aa866b04bb5ef94b965ec429d617963f0e78c
Gabriel <gkime91@gmail.com>
2024-01-19 23:57:03 -0800

    quicksort practice

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -27,18 +27,54 @@ function quicksort(array, start = 0, end = array.length - 1) {
  * @param {int} end partition end
  */
 
+// function partition(array, start, end) {
+//   const pivotValue = array[end];
+//   let pivotIndex = start;
+
+//   for (let i = start; i < end; i++) {
+//     if (array[i] < pivotValue) {
+//       [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+//       pivotIndex++;
+//     }
+//   }
+//   [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
+//   return pivotIndex;
+// }
+
 function partition(array, start, end) {
   const pivotValue = array[end];
-  let pivotIndex = start;
-  for (let i = start; i < end; i++) {
-    if (array[i] < pivotValue) {
-      [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
-      pivotIndex++;
+
+  let i = start;
+  let j = end;
+
+  // move i towards j until i is equal to j
+  while (i <= j) {
+    if (array[i] <= pivotValue) {
+      i++;
+    }
+    if (array[j] >= pivotValue) {
+      j--;
+    }
+
+    if (array[i] > pivotValue && array[j] < pivotValue) {
+      [array[i], array[j]] = [array[j], array[i]];
     }
   }
 
-  [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
-  return pivotIndex;
+  [array[i], array[end]] = [array[end], array[i]];
+
+  return i;
+
+  // if array[j] is greater than pivot value
+  // move j towards middle
+  // if array[i] is less than pivot value
+  // move i towards middle
+
+  // if array[i] is greater than pivotValue and
+  // array[j] is less than pivotValue
+  // swap
+
+  // insert pivot value at i
 }
 
 quicksort(unsorted);
