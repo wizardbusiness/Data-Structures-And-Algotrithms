a3997efdef10c89fed7087adcb9b307668ac71a0
Gabriel <gkime91@gmail.com>
2024-01-20 23:28:21 -0800

    practice depthfirstsearch

--- dataStructures/graph.js
+++ dataStructures/graph.js
@@ -14,18 +14,17 @@ const directionalGraph = {
  * structure: stack
  */
 
-const depthFirst = (graph, startNode, print = "") => {
-  const currNode = startNode;
-  const neighbors = graph[currNode];
-  neighbors.forEach((node) => {
-    console.log(node);
-    depthFirst(graph, node);
-  });
-  return print;
+const depthFirstSearch = (graph, startNode) => {
+  const stack = [graph[startNode]];
+  const currNode = stack.pop();
+  for (const neighbor of currNode) {
+    stack.push(neighbor);
+    console.log(neighbor);
+    depthFirstSearch(graph, neighbor);
+  }
 };
 
-depthFirst(directionalGraph, "a");
-
+depthFirstSearch(directionalGraph, "a");
 /**
  * breadth first traversal
  * explore all neighbors of a node before searching additional nodes.
@@ -37,52 +36,3 @@ depthFirst(directionalGraph, "a");
  * @param {*} graph
  * @param {*} startNode
  */
-// const depthFirstTraverse = (graph, startNode) => {
-//   // stack
-//   let printedNodes = "";
-//   const stack = [startNode];
-//   while (stack.length) {
-//     // pop off the current node
-//     let currNode = stack.pop();
-//     // examine its neighbors and push them onto the stack.
-//     const neighbors = graph[currNode];
-//     neighbors.forEach((neighbor) => {
-//       stack.push(neighbor);
-//     });
-
-//     stack.length === 0
-//       ? (printedNodes += `${currNode}`)
-//       : (printedNodes += `${currNode} --> `);
-//   }
-
-//   return printedNodes;
-//   // pick starting node.
-//   // push starting node onto stack.
-//   // pop it off, then push its neighbors to the stack.
-//   // order they are pushed is arbitrary and doesn't matter.
-//   // pop off top of stack, and push its neighbors onto the stack.
-// };
-
-// const depthFirstPrint = depthFirstTraverse(directionalGraph, "a");
-// console.log(depthFirstPrint);
-
-// const breadthFirstTraversal = (graph, startNode) => {
-//   let printedNodes = "";
-//   // queue
-//   const queue = [startNode];
-//   while (queue.length) {
-//     const currNode = queue.shift();
-//     const neighbors = graph[currNode];
-//     neighbors.forEach((neighbor) => {
-//       queue.push(neighbor);
-//     });
-//     queue.length === 0
-//       ? (printedNodes += `${currNode}`)
-//       : (printedNodes += `${currNode} --> `);
-//   }
-
-//   return printedNodes;
-// };
-
-// const breadthFirstPrint = breadthFirstTraversal(directionalGraph, "a");
-// console.log(breadthFirstPrint);
