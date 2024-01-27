5aeff2cb87b9f96bf250c932ba1f3e24543e9e26
Gabriel <gkime91@gmail.com>
2024-01-26 23:05:36 -0800

    quicksort practice

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -1,69 +1,48 @@
 const unsorted = [0, 5, 3, 2, 1, 10, 100, -100, 200, 19, 2, -200];
 const unsorted2 = [20];
-
 /**
  * @function quicksort
- * @description sorts an array of ints from smallest to greatest
- * using a pivot element to divide and conquer and then partition the array in O(nlogn) time
- * @param {int[]}
+ * @param {int[]} array
  * @param {int} start - partition start
  * @param {int} end - partition end
+ * @description sorts an unsorted array of ints in place
+ *
  */
 
 function quicksort(array, start = 0, end = array.length - 1) {
   if (start >= end) return;
 
-  const pivotIndex = partition1(array, start, end);
+  const partitionIndex = partition(array, start, end);
 
-  quicksort(array, start, pivotIndex - 1);
-  quicksort(array, pivotIndex + 1, end);
+  quicksort(array, start, partitionIndex - 1);
+  quicksort(array, partitionIndex + 1, end);
 }
 
 /**
  * @function partition
- * @description swaps elements inside a partition around a pivot value and finds the correct pivot index
- * @param {int[]}
- * @param {int} start partition start
- * @param {int} end partition end
+ * @param {int[]} array
+ * @param {int} start
+ * @param {int} end
+ * @returns {int} pivot index
+ * @description partitions elements in an array of ints around a pivot and returns the pivot index
  */
 
-// for loop partition method
-function partition1(array, start, end) {
+function partition(array, start, end) {
+  // pivotValue
   const pivotValue = array[end];
+  // pivot index
   let pivotIndex = start;
+
   for (let i = start; i < end; i++) {
     if (array[i] < pivotValue) {
+      // swap elements at i and pivot index
       [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+      // increment pivot index
       pivotIndex++;
     }
   }
-
   [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
   return pivotIndex;
-}
-
-// while loop partition method
-function partition2(array, start, end) {
-  const pivotValue = array[end];
-
-  let i = start;
-  let j = end;
-
-  while (i <= j) {
-    do {
-      i++;
-    } while (array[i] < pivotValue);
-    do {
-      j--;
-    } while (array[j] > pivotValue);
-
-    if (i <= j) {
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
-
-  [array[i], array[end]] = [array[end], array[i]];
-  return i;
 }
 
 quicksort(unsorted);
