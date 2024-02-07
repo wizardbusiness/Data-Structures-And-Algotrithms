96b439ee11f3dcbb25f6d087efd0e59d6f121a49
Gabriel <gkime91@gmail.com>
2024-02-07 11:48:14 -0800

    practiced mergesort

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -1,3 +1,5 @@
+// https://stackabuse.com/merge-sort-in-javascript/
+
 const unsorted = [0, 3, 1, 4, -2, 10, 8, 31];
 /**
  * @function mergesort
@@ -10,31 +12,27 @@ const unsorted = [0, 3, 1, 4, -2, 10, 8, 31];
 function mergesort(array) {
   if (array.length <= 1) return array;
   const mid = Math.floor(array.length / 2);
-  const l = mergesort(array.slice(0, mid)); // returns sorted left side
-  const r = mergesort(array.slice(mid)); // returns sorted right side
+  const l = mergesort(array.slice(0, mid));
+  const r = mergesort(array.slice(mid));
 
   return merge(l, r);
 }
 
 /**
  * @function merge
- * @param {int[]} l
- * @param {int[]} r
- * @description
- * compares elements in two input arrays and sorts them into a new array
+ * @param {int[]} l left half of array to be merged
+ * @param {int[]} r right half of array to be merged
+ * @description merges two unsorted arrays into a single sorted array
  */
 
 function merge(l, r) {
   const sorted = [];
   while (l.length && r.length) {
-    if (l[0] < r[0]) {
-      sorted.push(l.shift());
-    } else if (r[0] <= l[0]) {
-      sorted.push(r.shift());
-    }
+    if (l[0] < r[0]) sorted.push(l.shift());
+    else if (r[0] <= l[0]) sorted.push(r.shift());
   }
 
-  return [...sorted, ...l, ...r];
+  return [...sorted, ...l, ...r]; // any remainders at end, we don't know what values they contain
 }
 
 console.log(mergesort(unsorted));
