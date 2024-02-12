a65e622d71e614f9d97a89f121857e4c19de07ce
Gabriel <gkime91@gmail.com>
2024-02-12 10:29:52 -0800

    mergesort review

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -1,41 +1,47 @@
 // https://stackabuse.com/merge-sort-in-javascript/
 
 const unsorted = [0, 3, 1, 4, -2, -10, 2, 10, 8, 31];
+
 /**
  * @function mergesort
  * @param {int[]} array
+ * @returns {int[]}
  * @description
- * https://stackabuse.com/merge-sort-in-javascript/
- * sorts an array of elements in Onlogn time using divide and conquer and merge
+ * divides and conquers an unsorted array and returns a sorted array
  */
 
 function mergesort(array) {
+  // base case - arrays with 0 or 1 elements are sorted
   if (array.length <= 1) return array;
-
-  const midpoint = Math.floor(array.length / 2);
-
-  const l = mergesort(array.slice(0, midpoint));
-  const r = mergesort(array.slice(midpoint));
+  // divide and conquer
+  const mid = Math.floor(array.length / 2);
+  const l = mergesort(array.slice(0, mid));
+  const r = mergesort(array.slice(mid));
 
   return merge(l, r);
 }
 
 /**
  * @function merge
- * @param {int[]} l left half of array to be merged
- * @param {int[]} r right half of array to be merged
- * @description merges two unsorted arrays into a single sorted array
+ * @param {int[]} l
+ * @param {int[]} r
+ * @returns {int[]}
+ * @description
+ * Sorts left and right input arrays into a single sorted array
+ * and returns the result
  */
 
 function merge(l, r) {
   const sorted = [];
+
   while (l.length && r.length) {
+    let smallerVal;
     if (l[0] < r[0]) {
-      const smaller = l.shift();
-      sorted.push(smaller);
+      smallerVal = l.shift();
+      sorted.push(smallerVal);
     } else {
-      const smaller = r.shift();
-      sorted.push(smaller);
+      smallerVal = r.shift();
+      sorted.push(smallerVal);
     }
   }
 
