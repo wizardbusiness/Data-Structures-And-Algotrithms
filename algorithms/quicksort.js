785254a76ecd6c82e29d843b5d51857378d5bc2f
Gabriel <gkime91@gmail.com>
2024-02-19 22:40:55 -0800

    quicksort review

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -1,16 +1,22 @@
 const unsorted = [0, 5, 3, 2, 1, 10, 100, -100, 200, 19, 2, -200];
 const unsorted2 = [20];
+
 /**
  * @function quicksort
  * @param {int[]} array
  * @param {int} start
  * @param {int} end
+ * @description Onlogn in place sort
  */
 
-function quicksort(array, start = 0, end = array.length - 1) {
+function quicksort(array, start, end) {
+  if (!start || !end) {
+    console.error("Error: Start or end value not entered!");
+    return;
+  }
   if (start > end) return;
 
-  let pivotIndex = swap(array, start, end);
+  const pivotIndex = swap(array, start, end);
 
   quicksort(array, start, pivotIndex - 1);
   quicksort(array, pivotIndex + 1, end);
@@ -21,10 +27,13 @@ function quicksort(array, start = 0, end = array.length - 1) {
  * @param {int[]} array
  * @param {int} start
  * @param {int} end
+ * @return {int} pivotIndex
+ * @description swaps elements in On time and returns pivot index
  */
 
 function swap(array, start, end) {
   const pivotValue = array[end];
+  // increment to correct location
   let pivotIndex = start;
 
   for (let i = start; i < end; i++) {
@@ -33,10 +42,9 @@ function swap(array, start, end) {
       pivotIndex++;
     }
   }
-
   [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
-
   return pivotIndex;
 }
+
 quicksort(unsorted);
 console.log(unsorted);
