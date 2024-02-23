ec15513ddcaecb016596c965cc5f5c9a00d5a01b
Gabriel <gkime91@gmail.com>
2024-02-23 14:37:23 -0800

    mergesort review

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -4,38 +4,41 @@ const unsorted = [0, 3, 1, 4, -2, -10, 2, 10, 8, 31];
 
 /**
  * @function mergesort
- * @param {int[]} array
- * @returns {int[]}
- *
- * sorts an array of values from least to greatest in Onlogn
+ * @param {int[]} array unsorted
+ * @return {int[]} sorted
+ * @description O(nlogn) time O(nlogn) space stable sort
  */
 
 function mergesort(array) {
-  if (array.length <= 1) return array;
-  const mid = Math.floor(array.length / 2);
+  const mid = array.length / 2;
 
+  // base
+  if (array.length <= 1) return array;
+  // REMEMBER TO ACTUALLY CALL MERGESORT HAHAHA
   const l = mergesort(array.slice(0, mid));
   const r = mergesort(array.slice(mid));
 
+  // merge
   return merge(l, r);
 }
 
 /**
  * @function merge
- * @param {int[]} l unsorted array
- * @param {int[]} r unsorted array
- * compares elements from two input arrays and orders them from least to greatest in a sorted array
+ * @param {int[]} l unsorted
+ * @param {int[]} r unsorted
+ * @returns {int[]} [l < r , l, r]
+ * @description O(n) time O(n) space queue and merge operation
  */
 
 function merge(l, r) {
   const sorted = [];
-
   while (l.length && r.length) {
-    if (l[0] < r[0]) sorted.push(l.shift());
-    else sorted.push(r.shift());
+    if (l[0] < r[0]) {
+      sorted.push(l.shift());
+    } else {
+      sorted.push(r.shift());
+    }
   }
-
   return [...sorted, ...l, ...r];
 }
-
 console.log(mergesort(unsorted));
