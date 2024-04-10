5c0a65bd5a896d3d97e9509e417fd41b78428269
Gabriel <gkime91@gmail.com>
2024-04-10 12:22:21 -0700

    quicksort review

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -4,37 +4,37 @@ const unsorted2 = [20];
 
 /**
  * @function quicksort
- * @param{number[]} array
+ * @param {number[]} array
  * @param {number} start
  * @param {number} end
- * @description recursively partitions array 
+ * @description sorts integers in an unsorted array in nlogn time and n space
  */
 
-function quicksort(array, start=0, end=array.length - 1) {
-  if (start >= end) return;
+function quicksort(array, start = 0, end = array.length - 1) {
+  if (start > end) return;
+
   const partitionIndex = swap(array, start, end);
+
   quicksort(array, start, partitionIndex - 1);
   quicksort(array, partitionIndex + 1, end);
 }
-/**
- * @function swap 
- * @param{number[]} array
- * @param {number} start
- * @param {number} end
- * @description swaps elements in partition and returns the partition index
- */
 
-function swap(array, start, end) {
-  const pivot = array[end]
-  let startIndex = start;
-  for (let i = start; i < array.length; i++) {
+function swap(array, start, end){
+  const pivot = array[end];
+
+  let pivotIndex = start;
+
+  for (let i = start; i < end; i++) {
     if (array[i] < pivot) {
-      [array[i], array[startIndex]] = [array[startIndex], array[i]]
-      startIndex++;
+      [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+      pivotIndex++;
     }
   }
-  [array[startIndex], array[end]] = [array[end], array[startIndex]];
-  return startIndex;
+
+  [array[pivotIndex], array[end]] = [array[end], array[pivotIndex]];
+  return pivotIndex;
 }
 
-quicksort(unsorted);
+quicksort(unsorted)
+
+console.log(unsorted)
