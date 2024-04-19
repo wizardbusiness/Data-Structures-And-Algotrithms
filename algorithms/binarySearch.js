a5304d6a1cb37c649305893284640679030b75e0
Gabriel <gkime91@gmail.com>
2024-04-18 23:16:52 -0700

    binary search review

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -8,19 +8,17 @@
  * @returns {number} - index of target number or -1
  */
 
-function binarySearch(array, target, start = 0, end = array.length - 1) {
-  if (start > end) return -1;
-
-  const mid = Math.floor((start + end) / 2);
-
-  if (target < array[mid]) {
+function binarySearch(array, target, start=0, end=array.length - 1) {
+  if (start > end) return -1
+  const mid = Math.floor((end + start) / 2);
+  if (target === array[mid]) {
+    return mid
+  } else if (target < array[mid]) {
     return binarySearch(array, target, start, mid - 1);
   } else if (target > array[mid]) {
     return binarySearch(array, target, mid + 1, end);
-  } else if (target === array[mid]) {
-    return mid;
-  }
+  } 
 }
 
 const testArray = [1, 2, 4, 6, 10, 12, 1000];
-console.log(binarySearch(testArray, 2)); // 3
+console.log(binarySearch(testArray, 1000)); // 1
