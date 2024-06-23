6b2dcc0e6cb7086496f5d4cca736ffad38d349c0
Gabriel <gkime91@gmail.com>
2024-06-22 19:26:53 -0700

    iterative and recursive dfs practice

---
+++ algorithms/graphs/depthFirstTraversal.js
@@ -0,0 +1,32 @@
+const graph = {
+  a: ["b", "c"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: []
+}
+
+// recursive
+const depthFirstSearch = (graph, source) => {
+  console.log(source);
+
+  for (let neighbor of graph[source]) {
+    depthFirstSearch(graph, neighbor);
+  }
+}
+
+
+const depthFirstSearchIterative = (graph, source) => {
+  const stack = [source];
+
+  while (stack.length > 0) {
+    const currNode = stack.pop();
+    console.log(currNode);
+    for (let neighbor of graph[currNode]) {
+      stack.push(neighbor);
+    }
+  }
+}
+depthFirstSearch(graph, "a")
+depthFirstSearchIterative(graph, "a")
