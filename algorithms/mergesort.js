08af78280a63501d78da93a6a092621373c47d9e
Gabriel <gkime91@gmail.com>
2024-06-22 17:17:35 -0700

    mergesort review

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -1,25 +1,24 @@
 // https://stackabuse.com/merge-sort-in-javascript/
 
-const mergeSort = (arr) => {
-  if (arr.length <= 1) return arr;
-  // recursively divide array in two
-  const sliceIndex = Math.floor(arr.length / 2);
-  const l = mergeSort(arr.slice(0, sliceIndex));
-  
-  const r  = mergeSort(arr.slice(sliceIndex));
-  
+const mergeSort = (array) => {
+  if (array.length <= 1) return array;
+  const mid = array.length / 2;
+
+  const l = mergeSort(array.slice(0, mid));
+  const r = mergeSort(array.slice(mid));
 
   return sort(l, r);
 }
 
 const sort = (l, r) => {
   const sorted = [];
-  while (l.length && r.length) {
+
+  while(l.length && r.length) {
     if (l[0] < r[0]) sorted.push(l.shift());
-    else sorted.push(r.shift());
+    else sorted.push(r.shift())
   }
 
   return [...sorted, ...l, ...r];
 }
 
-console.log(mergeSort([2, -1, 10, 15, 3, 1, 7, -10]));
+console.log(mergeSort([1, 2, 3, 2, 0, -12, 20, 4]));
