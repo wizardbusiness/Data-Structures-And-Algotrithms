866434d95e359398097d6bbf9fe699e69b4f86ae
Gabriel <gkime91@gmail.com>
2024-07-07 14:43:58 -0700

    dp practice with fib

--- algorithms/dynamicProgramming/fibonacci.js
+++ algorithms/dynamicProgramming/fibonacci.js
@@ -1,15 +1,16 @@
 // Write a function fib(n) that takes in a number as an argumen.
 // The function should return the n-th number of the Fib sequence. 
+// fib(n) = fib(n - 1) + fib(n - 2)
 
-const fib = (n, memo={}) => {
-  console.log(memo)
-  if (n <= 2) return 1;
-  if (n in memo) return memo[n];
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
-  return fib(n - 1, memo) + fib(n - 2, memo)
+
+const fib = (n, cache={}) => {
+  if (n in cache) return cache[n]
+  if (n === 0 || n === 1) return 1;
+  cache[n] = fib(n - 1, cache) + fib(n - 2, cache)
+  return cache[n];
 }
 
-console.log(fib(6));
+console.log(fib(4));
 console.log(fib(7));
 console.log(fib(8));
 console.log(fib(50));
