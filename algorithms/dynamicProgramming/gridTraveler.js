f00d30dc6f2b2dfc2bc444331c548db30c7431de
Gabriel <gkime91@gmail.com>
2024-07-07 14:27:53 -0700

    dp practice with gridtraveler

--- algorithms/dynamicProgramming/gridTraveler.js
+++ algorithms/dynamicProgramming/gridTraveler.js
@@ -1,22 +1,19 @@
 const gridTraveler = (m, n, cache={}) => {
   const key = m + ',' + n;
-  if (key in cache) return cache[key]
-  // if 0 x n grid there will be 0 ways to travel
+  if (key in cache) return cache[key];
   if (m === 0 || n === 0) return 0;
-  // if 1 x n grid there will be one way to travel the grid
-  if (m === 1 && n === 1) return 1;
-  
-  // always two options
-  // down and right
-  // down reduces number of rows by 1 
-  
-  const down = gridTraveler(m - 1, n, cache);
-  // right reduces number of columns by 1
-  const right = gridTraveler(m, n - 1, cache);
-  cache[key] = down + right;
-  return cache[key];
+  if (m === 1 || n === 1) return 1;
+
+
+  const right = gridTraveler(m - 1, n, cache);
+  const down = gridTraveler(m, n - 1, cache);
+
+  cache[key] = right + down
+  return right + down;
 }
 
-console.log(gridTraveler(2, 3)); // -> 3
-console.log(gridTraveler(2, 3));; // -> 3
+
+// unmemoized O(2 ^ n + m)
+// memoized O(n + m) -> 3
+console.log(gridTraveler(3, 3)); // -> 3
 console.log(gridTraveler(18, 18)); // -> 3
