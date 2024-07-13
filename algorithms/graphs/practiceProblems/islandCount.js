9737b4557f2c78d6451917120ea09d439cfe04d1
Gabriel <gkime91@gmail.com>
2024-07-12 23:30:55 -0700

    grid algo practice with island count

--- algorithms/graphs/practiceProblems/islandCount.js
+++ algorithms/graphs/practiceProblems/islandCount.js
@@ -10,9 +10,7 @@ const grid = [
 ];
 
 // think about an xy position as a node
-
 const islandCount = (grid) => {
-
   const visited = new Set();
   let count = 0;
 
@@ -26,23 +24,24 @@ const islandCount = (grid) => {
 }
 
 const exploreIsland = (grid, r, c, visited) => {
-  const tile = r + "," + c;
-  if (visited.has(tile)) return false;
-  visited.add(tile);
-  const rowInBound = r >= 0 && r < grid.length;
-  const colInBound = c >= 0 && c < grid[0].length;
-  if (!rowInBound || !colInBound) return false;
+  const coord = r + "," + c
+  if (visited.has(coord)) return false;
+  visited.add(coord);
+  const rowInbounds = 0 <= r && r < grid.length;
+  const columnInbounds = 0 <= c && c < grid[0].length; 
+  if (!rowInbounds || !columnInbounds) return false;
   if (grid[r][c] === "W") return false;
+
   // explore left
-  exploreIsland(grid, r, c - 1, visited); 
+  exploreIsland(grid, r, c - 1, visited);
   // explore right
   exploreIsland(grid, r, c + 1, visited);
   // explore up
   exploreIsland(grid, r - 1, c, visited);
-  // explore down
-  exploreIsland(grid, r + 1, c, visited);
+  // explore down 
+  exploreIsland(grid, r + 1, c, visited)
 
   return true;
-};
+}
 
 console.log(islandCount(grid));
