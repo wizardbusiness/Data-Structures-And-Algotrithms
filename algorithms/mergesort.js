a5e1566c369d9216a354f9b07a01b77865cf5b09
Gabriel <gkime91@gmail.com>
2024-07-18 11:45:46 -0700

    mergesort practice

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -1,24 +1,25 @@
 // https://stackabuse.com/merge-sort-in-javascript/
 
+const numArray = [1, 3, -3, 10, 4, -20, 100]
 const mergeSort = (array) => {
   if (array.length <= 1) return array;
-  const mid = array.length / 2;
-
+  const mid = Math.round(array.length / 2)
   const l = mergeSort(array.slice(0, mid));
   const r = mergeSort(array.slice(mid));
 
-  return sort(l, r);
+  return (merge(l, r));
 }
 
-const sort = (l, r) => {
+const merge = (l, r) => {
   const sorted = [];
 
-  while(l.length && r.length) {
-    if (l[0] < r[0]) sorted.push(l.shift());
+  while(l.length > 0 && r.length > 0) {
+    if (l[0] < r[0]) sorted.push(l.shift())
     else sorted.push(r.shift())
   }
 
   return [...sorted, ...l, ...r];
 }
 
-console.log(mergeSort([1, 2, 3, 2, 0, -12, 20, 4]));
+console.log(mergeSort(numArray));
+
