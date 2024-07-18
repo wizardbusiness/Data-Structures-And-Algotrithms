9af5a7bb35be87e9d1deb7118d42c9fad011ecb3
Gabriel <gkime91@gmail.com>
2024-07-18 11:10:05 -0700

    practicing dp with canSum

--- algorithms/dynamicProgramming/canSum.js
+++ algorithms/dynamicProgramming/canSum.js
@@ -1,15 +1,18 @@
 // check if some combination of positive numbers in an array add up to a target value
 // you can use any number any number of times
 
-const canSum = (target, array, memo={}) => {
-  if (target in memo) return memo[target];
+// naive
+const canSum = (target, array) => {
+  // strat - subtract element from target recursively until base case is hit
+  // base case - if target is 0 return true
   if (target === 0) return true;
   if (target < 0) return false;
-  for (let num of array) {
-    memo[target] = canSum(target - num, array);
-    if (memo[target] === true) return true;
+
+  for (let value of array) {
+    if (canSum(target - value, array) === true) return true;
   }
-  return false
+
+  return false;
 }
 // m = height of tree / target value
 // n = breadth of tree / array length
@@ -20,8 +23,24 @@ const canSum = (target, array, memo={}) => {
 // for space, look at the max space the array can take up
 // each layer of tree is a frame on the call stack
 // for time look at the max height of the call stack (m) to the the max length of the array (n)
-// since each 
 
+// optimizied
 
-console.log(canSum(30000, [7, 14, 1]));
+const canSumOptimized = (target, array, memo={}) => {
+  if (target in memo) return memo[target];
+  if (target === 0) return true;
+  if (target < 0) return false;
+
+  for (let value of array) {
+    const remainder = target - value;
+    if (canSumOptimized(remainder, array, memo) === true) {
+      memo[target] = true;
+      return true;
+    }
+  }
+  memo[target] = false;
+  return false;
+}
+console.log(canSumOptimized(3000, [7, 14])); // true
+
 
