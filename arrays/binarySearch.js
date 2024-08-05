6fdadc37064ac68f54b704d1710c0891365aed4a
Gabriel <gkime91@gmail.com>
2024-08-04 21:21:50 -0700

    practiced binary search

--- arrays/binarySearch.js
+++ arrays/binarySearch.js
@@ -1,24 +1,23 @@
 /** Divide and Conquer. Divide array repeatedly in half until either middle element is found or array is empty */
 
 /**
- * @param {number} target - target number
- * @param {number[]} array - sorted array
- * @param {number} start - starting index
- * @param {number} end - ending index
- * @returns {number} - index of target number or -1
+ * @function binarySearch
+ * @param { number } target
+ * @param { number[] } sortedArray
+ * 
+ * time complexity O(logn);
+ * space complexity O(logn);
  */
 
-function binarySearch(array, target, start=0, end=array.length - 1) {
-  if (start > end) return -1
-  const mid = Math.floor((end + start) / 2);
-  if (target === array[mid]) {
-    return mid
-  } else if (target < array[mid]) {
-    return binarySearch(array, target, start, mid - 1);
-  } else if (target > array[mid]) {
-    return binarySearch(array, target, mid + 1, end);
-  } 
+function binarySearch(target, array, i=0, j=array.length - 1) {
+  if (i > j) return -1;
+  const mid = Math.round((j + i) / 2);
+  console.log(mid)
+  
+  if(target === array[mid]) return mid;
+  else if (target < array[mid]) return binarySearch(target, array, i, mid - 1);
+  else if (target > array[mid]) return binarySearch(target, array, mid + 1, j);
 }
 
-const testArray = [1, 2, 4, 6, 10, 12, 1000];
-console.log(binarySearch(testArray, 1000)); // 1
+console.log(binarySearch(8, [1, 2, 3, 4, 5, 8]));
+
